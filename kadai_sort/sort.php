7fab36acc7d27f3827695a95b0566a5153279107
Taka104sugi <145530668+Taka104sugi@users.noreply.github.com>
2023-10-28 22:59:16 +0900

    Add files via upload

---
+++ kadai_sort/sort.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+
+        function sort_2way(&$array, $order) {
+            if ($order === true) {
+                sort($array); 
+            } elseif ($order === false) {
+                rsort($array); 
+            }
+        }
+
+        $nums = [15, 4, 18, 23, 10];
+
+        echo "昇順にソートします。<br>";
+        sort_2way($nums, true);
+        foreach ($nums as $num) {
+            echo $num . '<br>';
+        }
+
+        echo "降順にソートします。<br>";
+        sort_2way($nums, false);
+        foreach ($nums as $num) {
+            echo $num . '<br>';
+        }
+        
+        ?>
+    </p>
+</body>
+
+</html>
